9576f0659ecc5b1c423c0827753b02518a112e33
The-Flash <absaugustineflash@gmail.com>
2021-08-10 09:41:57 +0000

    Update main.py
    Export questions to CSV file

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 URL = "https://stackoverflow.com/questions"
-PAGE_LIMIT = 3
+PAGE_LIMIT = 4
 
 def build_url(base_url=URL, tab="newest", page=1):
-    return f"{base_url}?tab={tab}&page={page}" # example: stackoverflow.com/questions?tab=newest&page=1
+    return f"{base_url}?tab={tab}&page={page}"
 
 def scrape_page(page=1):
     """
-    Function to scrape a single page in stackoverflow
+    Function to scrape a single page in stack overflow
     """
     response = requests.get(build_url(page=page))
     page_questions = []
@@ -22,12 +23,12 @@ def scrape_page(page=1):
         answers_count = summary.find(class_="status").find("strong").text
         view_count = summary.find(class_="views").text.split()[0]
         page_questions.append({
-            "question": question,
+            "question":  question,
             "answers": answers_count,
             "views": view_count,
             "votes": vote_count
         })
-
+    
     return page_questions
 
 
@@ -41,6 +42,19 @@ def scrape():
         questions.extend(page_questions)
     return questions
 
+def export_data():
+    data = scrape()
+    with open("questions.csv", "w") as data_file:
+        fieldnames = ["answers", "question", "views", "votes"]
+        data_writer = csv.DictWriter(data_file, fieldnames=fieldnames)
+        data_writer.writeheader()
+        for d in data:
+            data_writer.writerow(d)
+        print("Done")
+        
+
+
 if __name__ == "__main__":
-    from pprint import pprint
-    pprint(scrape())
+    # from pprint import pprint
+    # pprint(scrape())
+    export_data()
